220bf9f629dbe7006d65bdcb8314a74256bcfb66
xiyangab <xiyangh@nycstudents.net>
2025-11-07 09:20:01 -0500

    afaf
    afaf

--- file5.py
+++ file5.py
@@ -4,9 +4,13 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
-def movie(m):
+def movie():
+    m = input("what move do you like")
+    b = 0
     for a in data:
-        if a['title'] in m:
+        if m.lower() in a['title'].lower():
             print(a['title'])
-            print(a['genres'])
-movie('M3GAN')
+            b += 1
+    if b == 0:
+        print("no results")
+movie()
